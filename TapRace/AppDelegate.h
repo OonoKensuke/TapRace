7a0e5a69b3bd35aa087cb71d05ff5ac10168ba37
Oono Kensuke <bakadeaho55@gmail.com>
2013-06-30 02:41:59 +0900

    Initial Commit

---
+++ TapRace/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  TapRace
+//
+//  Created by 大野 謙介 on 13/06/30.
+//  Copyright 大野 謙介 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ TapRace/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TapRace
+//
+//  Created by 大野 謙介 on 13/06/30.
+//  Copyright 大野 謙介 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
